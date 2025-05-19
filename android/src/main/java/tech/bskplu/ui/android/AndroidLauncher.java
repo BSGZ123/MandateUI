a00c0d7cf4132cc13bc21bb9fe9d19565e395d7a
BsKPLu <bianshuiguangzhong@gmail.com>
2025-05-19 22:38:23 +0800

    Initial commit

---
+++ android/src/main/java/tech/bskplu/ui/android/AndroidLauncher.java
@@ -0,0 +1,18 @@
+package tech.bskplu.ui.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import tech.bskplu.ui.Main;
+
+/** Launches the Android application. */
+public class AndroidLauncher extends AndroidApplication {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
+        configuration.useImmersiveMode = true; // Recommended, but not required.
+        initialize(new Main(), configuration);
+    }
+}
