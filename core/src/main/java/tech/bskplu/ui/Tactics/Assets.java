5fc6f5c3be7505bda690c6d457535a83b4362a87
BsKPLu <bianshuiguangzhong@gmail.com>
2025-06-28 12:23:06 +0800

    T-1-0 小战场素材准备完毕

---
+++ core/src/main/java/tech/bskplu/ui/Tactics/Assets.java
@@ -0,0 +1,11 @@
+package tech.bskplu.ui.Tactics;
+
+/**
+ * @ClassName: Assets
+ * @Description: 资源管理
+ * @Author BsKPLu
+ * @Date 2025/6/28
+ * @Version 1.1
+ */
+public class Assets {
+}
---
+++ core/src/main/java/tech/bskplu/ui/Tactics/TacticsPanel.java
@@ -0,0 +1,11 @@
+package tech.bskplu.ui.Tactics;
+
+/**
+ * @ClassName: TacticsPanel
+ * @Description: 小战场
+ * @Author BsKPLu
+ * @Date 2025/6/28
+ * @Version 1.1
+ */
+public class TacticsPanel {
+}
